139f8ecd23ae3cd7eac9341c410ffcba3f01941a
Steffen Lintz <z003ne3b@MD1K32DC.ad001.siemens.net>
2018-03-13 14:21:18 +0100

    Activity

---
+++ src/main/java/dhbw/wwi16b2/webbasiertedatenbankanwendungen/sportanwendung/jpa/Activity.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dhbw.wwi16b2.webbasiertedatenbankanwendungen.sportanwendung.jpa;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author z003ne3b
+ */
+@Entity
+@Table(name="ACTIVITY")
+public class Activity implements Serializable {
+    @Id
+    @GeneratedValue
+    private long id =0;
+    
+    @NotNull(message="Es muss eine Sportart ausgewählt sein")
+    private Sportart sportart= null;
+    
+    private int calories;
+    
+    @NotNull(message="Es muss ein Datum angeben werden")
+    private Date date;
+    
+    @NotNull(message="Es muss eine Dauer angegeben werden")
+    private int duration;
+    
+    private int rating;
+
+    public Activity(Date date, int duration, int rating) {
+        this.date = date;
+        this.duration = duration;
+        this.rating = rating;
+    }
+
+    public Activity(Date date, int duration) {
+        this.date = date;
+        this.duration = duration;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Sportart getSportart() {
+        return sportart;
+    }
+
+    public void setSportart(Sportart sportart) {
+        this.sportart = sportart;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+    
+    
+    
+    
+}
